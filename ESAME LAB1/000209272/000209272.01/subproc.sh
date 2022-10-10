b1999076b239e792a2498e7fb3126e7d09cca820
Riccardo Gennaro <75367744+lallo-unitn@users.noreply.github.com>
2022-10-10 10:48:08 +0200

    Added lab1 and lab2 exams

---
+++ ESAME LAB1/000209272/000209272.01/subproc.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+n=1;
+for n in $(seq 1 10); do
+    echo $(echo $BASHPID)
+    echo $(echo $BASHPID)
+    echo $(echo $BASHPID)
+done
---
+++ ESAME LAB1/000209272/000209272.02/alias.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+longest 1>/tmp/out.txt 2>/tmp/err.txt
